a5314a66c1fbdad9f4c1485426030a14294a0c56
Lalit Bharindwal <lalitbharindwal@gmail.com>
2024-01-14 21:39:45 +0530

    Updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 if(sessionStorage.getItem("emailid")==null){
-    document.getElementById("auth-btn").innerHTML = '<a href="auth-signin.html"><button class="auth_btn"><img src="assets-main/images/login.png" alt="" height="30">  Login</button></a>';
+    if(localStorage.getItem("emailid")==null){
+        document.getElementById("auth-btn").innerHTML = '<a href="auth-signin.html"><button class="auth_btn"><img src="assets-main/images/login.png" alt="" height="30">  Login</button></a>';
+    }else{
+        document.getElementById("auth-btn").innerHTML = '<a href="auth-signin.html"><button onclick="logout()" class="auth_btn"><img src="assets-main/images/login.png" alt="" height="30">  Logout</button></a>';
+    }
 }else{
     document.getElementById("auth-btn").innerHTML = '<a href="auth-signin.html"><button onclick="logout()" class="auth_btn"><img src="assets-main/images/login.png" alt="" height="30">  Logout</button></a>';
 }
